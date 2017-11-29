696aa69fa8887de1060e8273f4e53f4f5dba760b
iuri aranda <iuri.aranda@gmail.com>
2017-11-29 18:47:05 +0100

    Added trusted_cluster definde resource to create trusted cluster objects

---
+++ manifests/trusted_cluster.pp
@@ -0,0 +1,31 @@
+# == Define: teleport::trusted_cluster
+#
+define teleport::trusted_cluster (
+  $enabled        = true,
+  $token          = undef,
+  $tunnel_addr    = undef,
+  $web_proxy_addr = undef,
+) {
+  if ! defined(Class['teleport']) {
+    fail('You must include the teleport base class before using the trusted_cluster defined resource')
+  }
+
+  validate_bool($enabled)
+  validate_string($token)
+  validate_string($tunnel_addr)
+  validate_string($web_proxy_addr)
+
+  file { "/etc/teleport_trusted_cluster_${name}.yaml":
+    ensure  => file,
+    mode    => '0640',
+    owner   => 'root',
+    group   => 'root',
+    content => template('teleport/trusted_cluster.yaml.erb'),
+  } ~>
+  exec { "create_teleport_trusted_cluster_${name}":
+    command     => "${teleport::bin_dir}/tctl create /etc/teleport_trusted_cluster_${name}.yaml",
+    refreshonly => true,
+    returns     => [0, 1], # will return 1 if the cluster already exists
+    require     => Class['teleport::install']
+  }
+}
